3abfea7ab33eac62c0149de0a74830532c50c861
foyed <145137772+foyed@users.noreply.github.com>
2024-11-18 20:09:55 -0500

    Add files via upload

---
+++ Tester1.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class Tester1 {
+
+    public static void main(String[] args) {
+        int numNormalCounters = 3;
+        double simulationTime = 3600;
+        double arrivalRate = 0.5;
+        int maxItems = 20;
+
+        Queue<Checkout> checkouts = new LinkedList<>();
+        checkouts.add(new SupCheckout(5)); 
+        checkouts.add(new ExpCheckout(10)); 
+
+        for (int i = 0; i < numNormalCounters; i++) {
+            checkouts.add(new NormalCheckout(15)); 
+        }
+
+        Random random = new Random();
+        double currentTime = 0.0;
+
+        while (currentTime < simulationTime) {
+            if (random.nextDouble() < arrivalRate) {
+                int items = random.nextInt(maxItems) + 1;
+                Customer newCustomer = new Customer(currentTime, 0, items);
+                customerGoesToLines(newCustomer, checkouts); 
+            }
+            for (Checkout checkout : checkouts) {
+                checkout.customerAtCounter(currentTime);
+            }
+
+            currentTime++;
+        }
+        for (Checkout checkout : checkouts) {
+            checkout.Display(checkouts);
+        }
+    }
+
+   /*
+    * Puts the customer in their correct line
+    */
+    private static void customerGoesToLines(Customer customer, Queue<Checkout> checkouts) {
+      
+        int numItems = customer.getNumItems();
+        Checkout bestCheckout = null;
+
+        for (Checkout checkout : checkouts) {
+            if ((checkout instanceof SupCheckout && numItems <= checkout.getMaxItems()) ||
+                (checkout instanceof ExpCheckout && numItems <= checkout.getMaxItems()) ||
+                (checkout instanceof NormalCheckout)) {
+                
+                if (bestCheckout == null || checkout.numOfCustomers() < bestCheckout.numOfCustomers()) {
+                    bestCheckout = checkout;
+                }
+            }
+        }
+
+        if (bestCheckout != null) {
+            bestCheckout.addCustomer(customer);
+        }
+    }
+}
+
+
